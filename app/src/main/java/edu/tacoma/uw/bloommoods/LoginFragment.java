330b37ecfc582dba105c34b9b7c14ab93d5fcc1a
Maliha <hossainmaliha18@gmail.com>
2024-04-29 15:23:34 -0700

    register working, still need to finish login

--- app/src/main/java/edu/tacoma/uw/bloommoods/LoginFragment.java
+++ app/src/main/java/edu/tacoma/uw/bloommoods/LoginFragment.java
@@ -5,11 +5,16 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.Navigation;
-
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import edu.tacoma.uw.bloommoods.databinding.FragmentLoginBinding;
 import edu.tacoma.uw.bloommoods.databinding.FragmentRegisterBinding;
@@ -20,9 +25,10 @@ import edu.tacoma.uw.bloommoods.databinding.FragmentRegisterBinding;
  * create an instance of this fragment.
  */
 public class LoginFragment extends Fragment {
-
-
     private FragmentLoginBinding mBinding;
+    private UserViewModel mUserViewModel;
+
+    private static final String TAG = "LoginFragment";
 
     public void navigateToRegister() {
         Navigation.findNavController(getView())
@@ -34,6 +40,8 @@ public class LoginFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        mUserViewModel = new ViewModelProvider(getActivity()).get(UserViewModel.class);
         // Inflate the layout for this fragment
         mBinding = FragmentLoginBinding.inflate(inflater, container, false);
         return mBinding.getRoot();
@@ -42,8 +50,53 @@ public class LoginFragment extends Fragment {
 
     public void onViewCreated (@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        mUserViewModel.addResponseObserver(getViewLifecycleOwner(), response -> {
+            observeResponse(response);
 
+        });
+        mBinding.signinButton.setOnClickListener(button-> signin());
         mBinding.registerTextview.setOnClickListener(button -> navigateToRegister());
 
     }
+
+    public void signin() {
+        String email = String.valueOf(mBinding.emailEdit.getText());
+        String pwd = String.valueOf(mBinding.pwdEdit.getText());
+        Log.i(TAG, email);
+        mUserViewModel.authenticateUser(email, pwd);
+    }
+
+
+    private void observeResponse(final JSONObject response) {
+        if (response.length() > 0) {
+            if (response.has("error")) {
+                try {
+                    Toast.makeText(this.getContext(),
+                            "Error Authenticating User: " +
+                                    response.get("error"), Toast.LENGTH_LONG).show();
+
+                } catch (JSONException e) {
+                    Log.e("JSON Parse Error", e.getMessage());
+                }
+
+            } else if (response.has("result")) {
+                try {
+                    String result = (String) response.get("result");
+                    if (result.equals("success")) {
+                        Toast.makeText(this.getContext(), "User logged in", Toast.LENGTH_LONG).show();
+                    } else {
+                        Toast.makeText(this.getContext(), "User failed to authenticate", Toast.LENGTH_LONG).show();
+
+                    }
+                } catch (JSONException e) {
+                    Log.e("JSON Parse Error", e.getMessage());
+                }
+            }
+
+        } else {
+            Log.d("JSON Response", "No Response");
+        }
+
+    }
 }
+
--- app/src/main/java/edu/tacoma/uw/bloommoods/RegisterFragment.java
+++ app/src/main/java/edu/tacoma/uw/bloommoods/RegisterFragment.java
@@ -1,15 +1,23 @@
 package edu.tacoma.uw.bloommoods;
 
+import static android.content.ContentValues.TAG;
+
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.Navigation;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import edu.tacoma.uw.bloommoods.databinding.FragmentRegisterBinding;
 
@@ -19,10 +27,8 @@ import edu.tacoma.uw.bloommoods.databinding.FragmentRegisterBinding;
  * create an instance of this fragment.
  */
 public class RegisterFragment extends Fragment {
-
     private FragmentRegisterBinding mBinding;
-
-
+    private UserViewModel mUserViewModel;
 
     public void navigateToLogin() {
         Navigation.findNavController(getView())
@@ -33,17 +39,56 @@ public class RegisterFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        mUserViewModel = new ViewModelProvider(getActivity()).get(UserViewModel.class);
         // Instantiate the Binding object and Inflate the layout for this fragment
         mBinding = FragmentRegisterBinding.inflate(inflater, container, false);
         return mBinding.getRoot();
 
     }
-
-@Override
+    @Override
     public void onViewCreated (@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-    mBinding.loginTextview.setOnClickListener(button -> navigateToLogin());
 
+        mUserViewModel.addResponseObserver(getViewLifecycleOwner(), response -> {
+            observeResponse(response);
+
+        });
+        mBinding.registerButton.setOnClickListener(button -> signup());
+        mBinding.loginTextview.setOnClickListener(button -> navigateToLogin());
+    }
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        mBinding = null;
+    }
+
+    public void signup() {
+        String email = String.valueOf(mBinding.emailEdit.getText());
+        String pwd = String.valueOf(mBinding.pwdEdit.getText());
+        String user = String.valueOf(mBinding.usernameEdit.getText());
+        Log.i(TAG, email);
+        mUserViewModel.addUser(email, pwd, user);
+    }
+
+    private void observeResponse(final JSONObject response) {
+        if (response.length() > 0) {
+            if (response.has("error")) {
+                try {
+                    Toast.makeText(this.getContext(),
+                            "Error Adding User: " +
+                                    response.get("error"), Toast.LENGTH_LONG).show();
+
+                } catch (JSONException e) {
+                    Log.e("JSON Parse Error", e.getMessage());
+                }
+
+            } else {
+                Toast.makeText(this.getContext(),"User added", Toast.LENGTH_LONG).show();
+            }
+
+        } else {
+            Log.d("JSON Response", "No Response");
+        }
 
     }
 
